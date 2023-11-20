0af205c19c877fcff90a5e2dcfb06cfc71f36aee
Hema-Mathiy <140504893+Hema-Mathiy@users.noreply.github.com>
2023-11-20 19:07:48 +0530

    Add files via upload

---
+++ Tas-6_Q4.py
@@ -0,0 +1,12 @@
+# Stroing a value in a veriable
+number = 81922
+# Converting the number to string
+number = str(number)
+# Extracting the first digit and convert it to an integer
+first_digit = int(number[0])
+# Extracting the last digit and convert it to an integer
+last_digit = int(number[-1])
+# Adding both values
+addition = first_digit + last_digit
+# Printing the results
+print("Addition of first and last digit of the number is", addition)
